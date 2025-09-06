06c226624140724468967bd6012e521f383570eb
Vedant Khullar <vedantkhullar@gmail.com>
2025-09-07 00:52:15 +0530

    fairshare project setup

---
+++ fairshare/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">FairShare</h1>
+    </div>
+  )
+}
+
+export default App
